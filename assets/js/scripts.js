70a95ffd2aea0c43590f035d866e9ef0e4bd02fa
Raymond Yao <raymondyao28@yahoo.com>
2020-08-04 22:56:03 -0700

    Remove front page animation

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -18,16 +18,16 @@ $(function() {
 // All others
 $(document).ready(function() {
     // zoom in/zoom out animations
-    if ($(".container").hasClass('fadeOut')) {
-        $(".container").removeClass("fadeOut").addClass("fadeIn");
-    }
-    if ($(".wrapper").hasClass('fadeOut')) {
-        $(".wrapper").removeClass("fadeOut").addClass("fadeIn");
-    }
-    $(".zoombtn").click(function() {
-        $(".container").removeClass("fadeIn").addClass("fadeOut");
-        $(".wrapper").removeClass("fadeIn").addClass("fadeOut");
-    });
+    // if ($(".container").hasClass('fadeOut')) {
+    //     $(".container").removeClass("fadeOut").addClass("fadeIn");
+    // }
+    // if ($(".wrapper").hasClass('fadeOut')) {
+    //     $(".wrapper").removeClass("fadeOut").addClass("fadeIn");
+    // }
+    // $(".zoombtn").click(function() {
+    //     $(".container").removeClass("fadeIn").addClass("fadeOut");
+    //     $(".wrapper").removeClass("fadeIn").addClass("fadeOut");
+    // });
     // go up button
     $.goup({
         trigger: 500,
